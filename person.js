c14cf537098a5ec1c58e49d0fc97f091be9cd0ed
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-09 13:54:09 +0530

    Introduction

---
+++ person.js
@@ -0,0 +1,4 @@
+let name="Allen"
+let age = 34
+
+module.exports=name;
---
+++ script.js
@@ -0,0 +1,25 @@
+// let importedData=require("./person.js")
+
+// const {add,sub}=require("./calc.js")
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(calc);
+
+// add(3,3)
+// sub(9,6)
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+setTimeout(() => {
+    console.log("Hi")
+},0)
+
+console.log(4);
+console.log(5);
+
